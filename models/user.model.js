0847ef0e36aff09bb09654310e681ef12927a1c8
domingo <domingoc1593@gmail.com>
2020-12-20 16:38:55 -0500

    validacion para agregar mas pedidos

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const Product = require("../models/product.model");
 
 const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    product: { type: {CharacteristicsSchema} }
+    product: { type: [CharacteristicsSchema] }
 });
 
 var CharacteristicsSchema = new Schema(
