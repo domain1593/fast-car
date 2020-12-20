b9ee9ee3edd191ccd6076b0b0a7948b01d36ea3d
domingo <domingoc1593@gmail.com>
2020-12-19 20:57:52 -0500

    agregando proveedor para estado en login

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var dashboardController = require('../controllers/dashboard/dashboard.controller');
+const { ensureAuthenticated } = require('../providers/auth.provider');
 
-router.get('/', dashboardController.recoverUserData);
+router.get('/', ensureAuthenticated, dashboardController.goToDashboard);
 
 module.exports = router;
